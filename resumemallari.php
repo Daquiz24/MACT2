e1061cafade45f107650d8f950ca9d3011033041
janboss11 <siguacharlesjayson@gmail.com>
2024-03-05 17:31:01 +0800

    jan henley

---
+++ resumemallari.php
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+
+class Jan Henley Mallari extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Jan Henley Mallari'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              radius: 100,
+              backgroundImage: NetworkImage(
+
+' https://www.google.com/imgres?imgurl=https%3A%2F%2Fpreview.redd.it%2Fxrgrlfo71xx41.png%3Fauto%3Dwebp%26s%3Dd99dcaa812a1f7f64bbdcda3e1e27543984d03b1&tbnid=v-vvXTGGlo72vM&vet=12ahUKEwjXmpHXrNiEAxVzW_UHHTpmCTUQMygCegUIARDTAQ..i&imgrefurl=https%3A%2F%2Fwww.reddit.com%2Fr%2FMewing%2Fcomments%2Fggz6hz%2Fis_this_guy_even_real_apparently_he_is_been%2F&docid=JDhN5Wf_BlwJfM&w=699&h=756&q=giga%20chad%20mewing&client=ms-android-transsion&ved=2ahUKEwjXmpHXrNiEAxVzW_UHHTpmCTUQMygCegUIARDTAQ', // Replace with your image link
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Jan Henley Mallari',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'IT Student',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Summary:',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Coding, or programming, is the process of instructing computers to perform specific tasks by writing sequences of instructions using a programming language. It involves problem-solving, logical thinking, and creating algorithms to achieve desired outcomes. Coders use languages like Python, Java, or C++ to build software, websites, and applications, shaping the digital world we interact with daily.',
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Skills:',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '- Knows a little about Coding',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            Text(
+              '- Reliable',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            Text(
+              '- Mobile Gaming',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Experience:',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Information technology student At HCC',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            Text(
+              '- Developing Something….',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            Text(
+              '- Collaborated with geng geng',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Education:',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Bachelor of Science in information technology',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            Text(
+              'HCC',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Projects:',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '- Still trying my best to learn ',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            Text(
+              '- Wanted to create a System that Can ….',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Certifications:',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '- Prefer not to say',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Languages:',
+              style: TextStyle( 
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '- English (Fluent)',
+              style: TextStyle(fontSize: 16.0),
+            ),  
+            Text(
+              '- Spanish (Intermediate)',
+              style: TextStyle(fontSize: 16.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  
+}

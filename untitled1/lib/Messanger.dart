0b3fe24c234fbe22d796549d4e6159e7f8676c10
daquiz24 <daquizcunananmark@gmail.com>
2024-03-06 22:05:55 +0800

    Daquiz Update

---
+++ untitled1/lib/Messanger.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MessangerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Massanger Page'),
+      ),
+      body: Center(
+        child: Text('This is the Messanger Page'),
+      ),
+    );
+  }
+}
